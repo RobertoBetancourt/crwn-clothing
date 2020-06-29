2094b41b4cad44bbf6505d2f47c7742d98605861
RobertoBetancourt <rob_betancourt@hotmail.com>
2020-06-29 09:14:19 -0500

    Adding sign in, form input and custom button components

--- src/App.js
+++ src/App.js
@@ -3,16 +3,10 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
-
 import HomePage from './pages/homepage/homepage.component';
 import ShopData from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
@@ -21,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopData} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
